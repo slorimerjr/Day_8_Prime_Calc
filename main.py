4026d6d326966e45f36cd7fa2d49fa575fdbaae2
SamLorimer1 <slorimerjr@gmail.com>
2021-11-22 22:12:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#Write your code below this line 👇
+def prime_checker(number):          
+    f = 2
+    s = []
+    while f < number:
+        if number % f == 0:
+            s.append(f)
+        f += 1
+    if len(s) > 0:
+        print("It's not a prime number.")
+    else:
+        print("It's a prime number.")
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
